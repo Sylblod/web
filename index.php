143c20cbd8cbd713569f67cc48b2a79a0950cefc
Sylblod <inquisidor.edo@gmail.com>
2026-02-24 15:55:44 -0600

    feat: example

--- index.php
+++ index.php
@@ -1,3 +1,54 @@
-<?php 
-echo "Hello World!";    
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta name="description" content="Hola mundo" />
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <div class="contenedor_logo ancho mar">
+        <a href="https://www.tecnm.mx/" target="_blank"><img src="imagenes/LogT1.jpg" alt="TecLargo"></a>
+        <a href="https://www.merida.tecnm.mx/" target="_blank"><img class="logotec" src="imagenes/LogT2.png" alt="TecCorto"></a>
+
+    </div>
+
+    <div class="contenedor_menu ancho mar">
+        <a href="index.html" class="links">Inicio</a>
+        <a href="conocenos.html" class="links">Conocenos</a>
+        <a href="carreras.html" class="links">Carreras</a>
+        <a href="https://tecvi2.itmerida.mx/my/" target="_blank" class="links">TECNM</a>
+        <a href="mailto:inquisidor.edo@gamil.com" target="_blank" class="links">corre</a>
+        <a href="mailto:inquisidor.edo@gamil.com?subject=MENSAJE DE PAGINA WEB" target="_blank" class="links">corre</a>
+        <a href="tel:9994969442" target="_blank" class="links">llamanos</a>
+
+
+    </div>
+
+    <div class="slide_foto ancho">
+        <img src="imagenes/LogT3.jpg" alt="slide_foto">
+
+    </div>
+
+    <div class="contenedor_bienvenida ancho mar">
+        <h1>Bienvenida al <span class="azul">TECNM</span> - <span class="rojo">MERIDA</span></h1>
+        <br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eum quaerat minima sunt hic obcaecati provident sapiente culpa, expedita blanditiis facilis iusto incidunt neque beatae odit nesciunt labore accusamus dolorem.</p>
+        <br>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem consectetur enim provident adipisci commodi? Ab id odio doloremque ut deserunt, illo quo, molestiae fugiat quam error quod delectus, iure ad.</p>
+        <br>
+        <a href="archivos/Archivo1.pdf" target="_blank" class="btn_rojo">Más informacion</a>
+        <br>
+    </div>
+    
+    <div class="contenedor_final ancho mar">
+        <img src="imagenes/LogT4.svg" alt="gobe">
+    </div>
+
+
+
+
+
+</body>
+</html>
